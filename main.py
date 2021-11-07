2574fe1cc37f655dddffd3a1fc98e0e3a00f6f53
WMF00 <wfriedrich593@g.rwu.edu>
2021-11-07 10:42:31 -0500

    Assignment Module 2.2

---
+++ main.py
@@ -0,0 +1,16 @@
+
+import os
+import re
+
+def regex():
+    txt = "The apples are ripe his time of year"
+    x = re.search("The", txt)
+    print(x)
+
+def regex2()
+
+if __name__ == '__main__':
+    regex()
+
+
+
